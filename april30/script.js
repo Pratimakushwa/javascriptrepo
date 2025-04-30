1c49de9a24eabcab8ea2b5c60b19477cd0fe3235
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-04-30 13:26:35 +0530

    javascript

---
+++ april30/script.js
@@ -0,0 +1,9 @@
+alert("hello friends")
+console.log("hello world")
+console.log("i hope you are understanding");
+/*
+this is multiple line cmment
+ */
+
+console.warn("this is warning")
+console.error("this is error")
