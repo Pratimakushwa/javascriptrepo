be20be18cf7bf5bfa27fd9fc716f196eacd08d3d
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-01 13:30:21 +0530

    1may

---
+++ 1may/script.js
@@ -0,0 +1,29 @@
+var name = "john"
+var age = 20;
+console.log(name);
+console.log(age);
+
+var x;
+console.log(x, typeof x);
+
+var firstName ="harry";
+console.log(firstName,typeof firstName);
+
+var num = 34.5;
+console.log(num, typeof num);
+
+var value = null;
+console.log(null, typeof null);
+//null datatype - object (mistake)
+
+var bolean = false;
+console.log(bolean, typeof bolean);
+
+var number = 3455n;
+console.log(number, typeof number);
+
+var id = Symbol(id);
+console.log(id, typeof id);
+
+
+
