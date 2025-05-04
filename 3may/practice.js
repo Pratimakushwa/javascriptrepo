9175d37f533f65b8eaaa01a91b4c07e84f39117b
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-04 14:30:50 +0530

    string

---
+++ 3may/practice.js
@@ -0,0 +1,13 @@
+let mypractice="javascript is most important  is for developer"
+// console.log(mypractice.trim());
+
+// console.log(mypractice.slice(-1,4));
+// console.log(mypractice.substring(-1,-4));
+// console.log( mypractice.split("i"));
+// /console.log(mypractice.charAt(9));
+console.log(mypractice.includes("for"));
+ console.log(mypractice.match("is"));
+
+
+
+
