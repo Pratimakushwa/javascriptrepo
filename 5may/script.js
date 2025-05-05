28eab30b5fe9c8a783a01e3779278d3e85dade13
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-05 13:31:55 +0530

    math

---
+++ 5may/script.js
@@ -0,0 +1,86 @@
+//math
+// let x = Math.PI
+// console.log(x);
+// // return positive
+// x =Math.abs(-72)
+// console.log(x);
+
+// //return -1 if num is negative, return 1 if num is positive
+//  x = Math .sign(6);
+//  console.log(x);
+//   //return square
+//    x = Math.sqrt(49)
+//    console.log(x);
+   
+//    //round 
+//    x= Math.round(4.56)
+//    console.log(x);
+//    //floor
+//    x= Math.floor(4.96)
+//    console.log(x);
+//    //ceil
+//    x= Math.ceil(4.16)
+//    console.log(x);
+//    //trunc
+//    //return integer
+//    x= Math.trunc(42343413.5634555)
+//    console.log(x);
+//    //min return the minimum value
+//    x = Math.min(45, -56 ,8 -345,0)
+//    console.log(x);
+//    //max return the minimum value
+//    x = Math.max(45, -56 ,98987, -345,0) 
+//    console.log(x);
+//    //return x to the power of (x,y)
+//    //min return the minimum value
+//    x = Math.pow(2,3)
+//    console.log(x);
+//    //random value between 0 and 1
+//    x = Math.random();
+//    //random num from 0 to 10
+//    x = Math.floor(Math.random()*10)
+//    console.log(x);
+//    //1 se start hoga
+//       x = Math.floor(Math.random()*10)
+//    console.log(x)
+
+//////
+  let x  = Math.floor(Math.random()*50)+1
+   let  y= Math.floor(Math.random()*100)+1
+   let sum = x+y
+   let product = x*y
+   let difference= x/y
+   let  questient = x+y
+
+   console.log( x+y)
+   console.log( x*y);
+   console.log(x/y);
+   console.log( x%y);
+   
+   //by default , javascript will use the browser's time zone and display a date 
+    //as full text string
+    console.log(d);
+    //there are 4 ways to create a new date object
+    //new date ()
+    //new date (year ,month,days,hours, second , milisecond)
+    //new date (milisecond)
+    //new date(date string)
+    //month - 0 to 11
+    d = new date (2020,4,3,23,34,53 ,898 );
+    d = new date (2020,4,3,23,34,53 , );
+
+    d = new date (2020,4,3,23,34, );
+
+    d = new date (2020);
+    //milisecond
+    d = new date(4854853);
+    console.log(d);
+    
+
+
+
+
+    
+
+   
+ 
