8b9cca020a856db6237999320166c20694b698c6
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-12 13:28:15 +0530

    array

---
+++ 11may/script.js
@@ -0,0 +1,60 @@
+//  //spread operator(..)
+//  //it copies  a part of an array to another array
+//  const num1 = [10,20,30,40]
+//  const num2=[60 ,70 ,80,]
+//  const num3 =[num1,...num2]
+//  console.log(num3);
+//  // isArray, .from
+//  const x = Array.isArray(num1);
+//  console.log(x);
+//   const y = Array .from ('12345');
+//   console.log(y);
+//   //destructuring 
+//   // breaking the variable in sinfle line
+//   const arr = [1,2,3,,4,5,6,7,8,9];
+//   //const a = arr[0];
+//   // const b = arr[1]
+//   const [ a,b,c,d,... rest]= arr;
+//   console.log(a,b,c,);
+  
+ 
+// const arr=[1,2,3,4,5]
+// arr.push(6)
+// arr.reverse()
+// arr.push(0)
+// console.log(arr);
+
+// const arr1=[1,2,3,4,5]
+// const arr2=[5,6,7,8,9,10]
+// arr2.shift()
+// const arr3=[...arr1,...arr2]
+// console.log(arr3);
+
+ const person ={
+    name: "john",
+    age : 45,
+    isEligiable :true,
+    address : {
+        state : "mp",
+        country : "India"
+    },
+    hobbies : ["dance", "singing"],
+    greet: function(){
+        console.log("hello");
+    }
+ }
+
+ console.log(person.name);
+ console.log(person['name']);
+ console.log(person.isEligiable);
+ console.log(person.address.country);
+ console.log(person.hobbies[1]);
+ person.greet();
+ 
+ 
+ 
+
+
+ 
+ 
+ 
