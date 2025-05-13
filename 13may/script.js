d204fc5c34b7ec6515007509bbd0631baf0000d5
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-13 13:28:38 +0530

    loop

---
+++ 13may/script.js
@@ -0,0 +1,11 @@
+// let x=("apple","banana","mango")
+// console.log(x)
+
+
+// let d = new Date();
+// console.log(d.toLocaleDateString());
+
+
+//  let x  = Math.floor(Math.random()*15)+1
+//  console.log(x);
+ 
--- 6may/script.js
+++ 6may/script.js
@@ -1,22 +1,22 @@
-// let d = new Date ();
-// // d= new Date ("2022-3-12 12:30:45");
-// // d= new Date ("12-3-2022");
-// // d = new Date("2022/5/13");
-// // d = new Date("249857565");
-// console.log(d);
-// //methods of dates
-// const year = d.getFullYear();
-// console.log(year);
-// const month= d.getMonth();
-// console.log(month);
-// const day= d.getDay();
-// console.log(day);
-// const hour= d.getHours();
-// console.log(hour);
-// const min= d.getMinutes();
-// console.log(min);
-// const sec= d.getSeconds();
-// console.log(sec)
+let d = new Date ();
+ d= new Date ("2022-3-12 12:30:45");
+ d= new Date ("12-3-2022");
+ d = new Date("2022/5/13");
+ d = new Date("249857565");
+console.log(d);
+//methods of dates
+const year = d.getFullYear();
+console.log(year);
+const month= d.getMonth();
+console.log(month);
+const day= d.getDay();
+console.log(day);
+const hour= d.getHours();
+console.log(hour);
+const min= d.getMinutes();
+console.log(min);
+const sec= d.getSeconds();
+console.log(sec)
 
  let d= new Date()
  console.log(d.getDay());
