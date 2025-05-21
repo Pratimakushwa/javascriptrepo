d75bdd545163a903a69c6b75a726805a46d4d09e
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-21 13:31:04 +0530

    py

---
+++ 21may/script.js
@@ -0,0 +1,41 @@
+//  then html dom model 
+// let h1 = document.getElementById("demo");
+// console.log(h1);
+// 
+// let h1 = document.getElementsByClassName("head");
+// console.log(h1[0]);
+
+// esme sare se tag se target kr skte hai
+// let h1 = document.querySelector("#demo");
+// console.log(h1);
+
+// let h1 = document.querySelectorAll("#demo");
+// console.log(h1);
+// let h1 = document.getElementById("demo")
+// h1.innerText= "hello world"
+
+// let name= 
+//     prompt(" enter  your name")
+//     h1.innterText="pratima"
+
+    
+
+// let h1 = document.getElementById("demo")
+// h1.innerText=" hello world "
+// h1.innerHTML= ` <p> hi i am form</p>`
+
+let h1 = document.getElementById("demo")
+h1.innerHTML = ` <table border= " "> 
+<tr>
+<th> name </th> 
+<th> age</th> 
+</tr>
+<tr>
+<td> pratima</td>
+<td> 22 </td>
+</tr>
+ </table>`
+ 
+ let img = document.getElementById("myimg");
+
+ img.src =" leaves.jpg";
