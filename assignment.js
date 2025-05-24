ea596aa399440a9ebeb8ef198a10732c278fb675
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-24 09:58:17 +0530

    js

---
+++ assignment.js
@@ -0,0 +1,43 @@
+1// How to get the first three character of string ?
+
+// let str= "HelloWorld";
+//  let firsTthree= str.substring(0,3);
+// console.log(firsTthree);
+
+2// delete first character of a string
+// let str= "HelloWorld";
+//  let  modified= str.substring(1);
+// console.log(modified);
+
+3// sort an array of string
+//  let arr=[ "banana", "apple", "cherry"]
+//  arr.sort();
+//  console.log(arr);
+
+4// Remove all break line from a  string
+// let str= "I\nlove\njavascript"
+// let breaks= str.split("\n")
+// console.log(breaks);
+
+5// extract a number from a string
+// let str= "hellow123";
+// let number= str.substring(6)
+// console.log(number);
+
+6// convert string to date 
+// let date= "2025-02-14";
+// let realDate =new Date(date);
+// console.log(realDate);
+
+7//insert string at specific index 
+// let original = "hello World ";
+// let toInsert= "Beautifull";
+// let index=5;
+// let result= original.substring(0,index)+toInsert+original.substring(index);
+// console.log(result);
+
+8//how to find the longest word within the string in javascript
+// let str =" dog elephant cat";
+// let words = str.split(' ')
+// let sorted = words. sort((a,b)=> b.length-a.length);
+// console.log(sorted);
