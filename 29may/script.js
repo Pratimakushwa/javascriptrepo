8810ed112d558efd17d0172e7da1249ac140e422
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-05-29 20:33:19 +0530

    js

---
+++ 29may/script.js
@@ -0,0 +1,64 @@
+// // 1
+// let str= ("javascipt")
+// let x=str.length;
+// console.log(x);
+
+// // 2
+
+// let str= ("javascript")
+// let x = str.toUpperCase()
+// console.log(x);
+
+3// let str= ("javascript great language")
+// let x = str.includes("great")
+// console.log(x);
+
+// 4 
+// let str= ("hello world")
+// let x = str.slice(6)
+// console.log(x);
+
+// 5
+// trim is used to remove white spaces of string
+// let str= ("    javascript  ")
+// let x = str.trim();
+// console.log(x);
+
+// 6 slice is used to pices of string  also posive or negative indexes
+//   substring  is same as slice  but is work negative index
+
+// 7 floor is  work less than 5
+
+// 8
+            
+// let x = Math.max[4,7,2,9]
+// console.log(x);
+
+
+
+ 
+//  let x = Math.floor(Math.random()*1)+1
+//    console.log(x);
+
+// 10 math ceil is work more then 5
+ 
+// 11 map is used to create new arry
+// for each method call function
+
+// 12 
+// let  str = [ "apple ", "banana",]
+//  let x= str.shift();
+// console.log(x);
+
+// 13
+// let arr= [ 1,3,1,5,2,6]
+// let x= arr.sort()
+// console.log(arr);
+
+
+ 
+
+ 
+
+
+
