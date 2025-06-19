2ac71e23e7877d9184cd3d184bc88145110ba1da
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-06-19 20:47:00 +0530

    html

--- 19june/script.js
+++ 19june/script.js
@@ -1,10 +1,19 @@
-for(let a = 1; a<=10; a++){
-    console.log(a)
-}
+// for(let a = 1; a<=10; a++){
+//     console.log(a)
+// }
 
-for (let a =2; a<=3; a++){
-    for (let b= 1; b<=10; b++){
-        console.log(a*b);
+// for (let a =2; a<=3; a++){
+//     for (let b= 1; b<=10; b++){
+//         console.log(a*b);
         
+//     }
+// }
+ for (let a = 1 ; a<=5; a++){
+    for (let b= 1; b<=5; b++){
+        if(a>=b){
+
+        document.writeln("*",edl)
+        }
+       
     }
-}
+ }
