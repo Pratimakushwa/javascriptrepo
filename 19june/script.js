a59db525bb11cd8d5b37b982e6ee18d5976eb1af
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-06-20 22:32:34 +0530

    html

--- 19june/script.js
+++ 19june/script.js
@@ -8,12 +8,41 @@
         
 //     }
 // }
- for (let a = 1 ; a<=5; a++){
-    for (let b= 1; b<=5; b++){
-        if(a>=b){
+//  for (let a = 1 ; a<=5; a++){
+//     for (let b= 1; b<=5; b++){
+//         if(a>=b){
 
-        document.writeln("*",edl)
-        }
+//         document.writeln("*",edl)
+//         }
        
-    }
- }
+//     }
+//  }
+
+//  let arry=["pratima", 3.15, 3, "b", ture]
+//  console.log(arr[3]);
+ 
+//  let arr=["pratima", 3.15, 3, "b", ture]
+//  for( let a =0; a<5; a++)
+//  console.log(arr[a]);
+ 
+
+
+let arr=[1,2,3,4,5,6,7,8,9,10]
+// for(let i=0; i<10; i++){
+//     console.log(arr[i]);
+    
+// }
+ 
+// for(let j=0; j<10; j++){
+//     if (arr[j]%2==0){
+//         console.log(arr [j]);
+        
+//     }
+// }
+
+
+// let arry=  [1,2,3,4,5,6,7,8]
+// for(let val of arry){
+//     console.log(val);
+    
+// }
