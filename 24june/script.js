d6e352da3264025ea332bd544c99ebae27f306e8
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-06-24 17:33:43 +0530

    html

---
+++ 24june/script.js
@@ -0,0 +1,53 @@
+// let object={
+//     name:"pratima",
+//     contact: 122344,
+//     city:"bhopal"
+
+// }
+// console.log(object);
+// console.log(object.name);
+
+
+//  let object={
+//     name:prompt("enter the name"),
+//     contact: prompt(" enter the number"),
+//     city:prompt("enter the city")
+
+// }
+// console.log(object.city);
+// console.log(object.name);
+
+
+let emp ={
+    emp_name: "pratima",
+    emp_id : "101"
+}
+emp.mail="@pratima23.com"
+emp.emp_name= "depali"
+console.log(emp);
+
+
+
+// let emp={
+
+
+//     emp_address: ["b178", "b17"],
+//     emp_contact:[152524,13656253]
+// }
+// console.log(emp.emp_address[1]);
+// console.log(emp.emp_contact[1]);
+
+// let obj={
+//     name:"pratima",
+//     city:["bhopal","indore"]
+// }
+
+// for (let k  in obj){
+    
+//     // console.log(k);//keys
+//     // console.log(obj[k]);//value
+//     console.log(k,":",obj[k]);//both
+    
+    
+// }
+// // print only one key
