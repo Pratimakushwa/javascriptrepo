18fbe9eb4060f0869a1216353a9d0408df699955
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-06-25 23:36:44 +0530

    html

---
+++ 25june/script.js
@@ -0,0 +1,62 @@
+// function add(){ //declaration 
+//     console.log( "inside the function");//defination
+    
+// }
+// add()
+// add()
+// add()
+// add()
+// add()
+
+// function sum(){
+//     let a=10
+//     let b=90
+//     console.log(a+b);
+    
+// }
+// sum()
+// sum()
+// sum()
+// function sum(){
+//     let a=prompt("enter the number the ")
+//     let b=prompt("enter the number the ")
+
+//     console.log(a-b);
+    
+// }
+// sum()
+// sum()
+// sum()
+
+
+//  function loop(){
+//     let a = prompt(" enter the number")
+//     for ( let i =1; i<=10; i++){
+//         console.log(a*i);
+        
+//     }
+  
+//  }
+//  loop()
+
+
+//with argument no return
+// function cloud( a){
+//     console.log(a*a*a);
+    
+
+// }
+// cloud(10)
+ function add(a){
+    if( a>0){
+        console.log("positive");
+        
+    }
+    else{
+        console.log("negative");
+        
+    }
+
+ }
+let b= prompt("enter the number")
+add(b)
