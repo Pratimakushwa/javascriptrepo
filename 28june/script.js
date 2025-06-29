7e35eb37b45a16e8a79f8c4534d44050fa274d9b
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-06-29 18:54:47 +0530

    js

---
+++ 28june/script.js
@@ -0,0 +1,27 @@
+// let h1= document.getElementById('head1')
+// h1.style.backgroundColor="blue"
+// h1.style.color="white"
+
+// // let c= document.getElementsByTagName('p')
+// // c[0].textContent="flipkart"
+
+// let h1= document.getElementById('head1')
+// h1.style.backgroundColor="yellow"
+// h1.style.color="red"
+
+// let b= document.getElementById('head2')
+// b.style.backgroundColor="blue"
+// b.style.color="pink"
+
+// let image =document.getElementById('image')
+image.src="fake.jpeg"
+// image.style.borderRadius="50%"
+
+let b= document.getElementById('head3')
+b.setAttribute('align','center')
+
+
+let head4= document.getElementById('head4')
+
+head4.removeAttribute('style')
+
---
+++ 29june/script.js
@@ -0,0 +1,19 @@
+// let a = document.getElementById('head1')
+// console.log(a);
+// a.textContent="inside js"
+// let b= document.getElementById('head2')
+// b.innerHTML="<i> cloud</i>"
+
+// let c= document.getElementsByClassName('head3')
+// c[0].textContent="change with js"
+// c[1].textContent="js"
+
+let c= document.getElementById('head1')
+c.style.backgroundColor="blue"
+c.style.color="grey"
+c.setAttribute('align','center')
+c.removeAttribute('style')
+let d= document.getElementById('head4')
+d.src="OIP.webp"
+d.style.borderRadius="50%"
+d.setAttribute('align','center')
