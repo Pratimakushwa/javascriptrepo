c0b934d0ae63bc40b661369f04d1490c618be88e
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-07-09 12:44:19 +0530

    js

--- 8may/script.js
+++ 8may/script.js
@@ -3,30 +3,45 @@
 //     alert("working")
 // })
 // let a=document.querySelector('#name')
-// a.addEventListener('')
+// a.addEventListener('contextmenu',function(){
+//     alert("working")
+// })
+// let b=document.querySelector('#task')
+// b.addEventListener('dblclick',function(){
+//     alert("working")
+// })
+// let c=document.querySelector('#fun')
+// c.addEventListener('mouseenter',function(){
+//     alert("working")
+// })
 
 
-let btn1=document.querySelector('#btn1')
-btn1.addEventListener('click',()=>{
-    let wr=document.querySelector('#wrapper')
-    wr.classList.add('main')
+// let btn1=document.querySelector('#btn1')
+// btn1.addEventListener('click',()=>{
+//     let wr=document.querySelector('#wrapper')
+//     wr.classList.add('main')
 
+// })
+
+
+// let btn2=document.querySelector('.btn2')
+// btn2.addEventListener('click',()=>{
+//     let wr =document.querySelector('#wrapper')
+//     wr.classList.remove('main')
+
+// })
+
+// let btn3=document.querySelector('.btn3')
+// btn3.addEventListener('click',function(){
+//     let wr =document.querySelector('#wrapper')
+//     wr.classList.toggle('main')
+
+// })
+
+
+let n= document.querySelector('.name')
+ n.addEventListener('click', function(){
+    let s= document.querySelector('#top')
+    s.classList.toggle('main')
 })
-
-
-let btn2=document.querySelector('.btn2')
-btn2.addEventListener('click',()=>{
-    let wr =document.querySelector('#wrapper')
-    wr.classList.remove('main')
-
-})
-
-let btn3=document.querySelector('.btn3')
-btn3.addEventListener('click',function(){
-    let wr =document.querySelector('#wrapper')
-    wr.classList.toggle('main')
-
-})
-
-
 
