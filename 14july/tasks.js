8837c0415db716d6208b3c1e646b626282de4c13
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-07-15 17:32:27 +0530

    js

--- 14july/tasks.js
+++ 14july/tasks.js
@@ -1,12 +1,44 @@
-let date2=new Date()
-let dt=document.querySelector('#date')
-dt.textContent=date2.getDate()
+// let date2=new Date()
+// let dt=document.querySelector('#date')
+// dt.textContent=date2.getDate()
 
-let day1=document.querySelector('#day')
-let arr=['sun','monday','tue','wed','thu','fri','sat']
-let day2=day2.getDay()
-day1.textContent=arr[day2]
-let m=document.querySelector('#month')
-m.textContent=date2.getMonth()+1
-let y=document.querySelector("#year")
-y.textContent=day2.getFullYear()
+// let day1=document.querySelector('#day')
+// let arr=['sun','monday','tue','wed','thu','fri','sat']
+// let day2=day2.getDay()
+// day1.textContent=arr[day2]
+// let m=document.querySelector('#month')
+// m.textContent=date2.getMonth()+1
+// let y=document.querySelector("#year")
+// y.textContent=day2.getFullYear()
+
+
+let date3=new Date()
+let date=document.querySelector('#date')
+date.textContent=date3.getDate()
+let left=document.querySelector('#left')
+left.style.backgroundColor="red"
+left.style.color="white"
+
+let day= document.querySelector('#day')
+let arry=["sun","mon","tue","wed","thu","fri", "sat"]
+let day3=date3.getDay()
+day.textContent=arry[day3]
+
+
+
+
+let months=document.querySelector('#month')
+let month=["jan","feb","march", "april","may","jun","july","august","september","october","november", "december"]
+let mm= date3.getMonth()
+months.textContent=month[mm]
+
+
+let right=document.querySelector('#right')
+right.style.backgroundColor="red"
+right.style.color="white"
+
+let year=document.querySelector('#year')
+year.textContent=date3.getFullYear()
+
+
+
---
+++ 15july/script.js
@@ -0,0 +1,41 @@
+let str="hacker space"
+console.log(str[1]);
+console.log(str.length);
+
+
+let an=str.at(-1)
+console.log(an);
+
+
+let student="me nhi pdhuga"
+console.log(student.repeat(10));
+
+// use to change sentance
+
+let str2="we are developer"
+ let rs=str2.replace('developer','SDE')
+ 
+ 
+
+ console.log(rs);
+ 
+
+ let str3=" vivek is very good student and also good in studies"
+ console.log(str3.replaceAll('good','bad'));
+ 
+
+ let str4="pushpendra"
+ console.log(str4.slice(0,5));
+
+//  any string  return arry
+
+ let str7='apple,mango,orange'
+ console.log(str7.split(','));
+ 
+ 
+ //templet litrals 
+ let fname="md"
+ let mname="quism"
+ let lname="khan"
+ console.log("my name"+" " + fname +" " + "mname+" " + lame);
+ 
