aa6d1994321e1327424794403866c12a7d6a67f0
Pratima kushwa <pratimakushwaha479@gmail.com>
2025-07-17 22:00:12 +0530

    js

---
+++ 17july/script.js
@@ -0,0 +1,35 @@
+// fetch => get  ,post , delete,put pending
+//promise -> fullfill,pending ,reject
+// http method
+// get post, delete, put ,patch
+
+// API  application programing inerface
+
+// fatch () => promise =>async  await is use get data
+
+// json // javascript object notesion
+
+async function demo(){
+    let response= await fetch("https://jsonplaceholder.typicode.com/comments")
+    let data = await response.json()
+    // console.log(data);
+    // let name= data.map((e)=>{ return e.name})
+    // console.log(name);
+    
+    let apidata =data.map((e)=>`
+    <tr>
+    <td>${e.id} </td>
+    <td> ${e.name} </td>
+    <td> ${e.email}</td>
+    </tr>
+`).join(" ")
+// console.log(apidata);
+
+let savedata=document.querySelector("#savedata")
+savedata.innerHTML=apidata
+   
+
+    
+}
+demo()
+
